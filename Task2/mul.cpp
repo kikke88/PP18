9dde0ed96b6ce24dc8f5082011867bca1d8d291b
Mokrov Kirill <kikke88@yandex.ru>
2018-10-15 22:16:18 +0300

    tmp2

--- Task2/mul.cpp
+++ Task2/mul.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 #include "papi.h"
 
-#define NUM_EVENTS 1
+#define NUM_EVENTS 2
 
 void handle_error (int retval)
 {
     printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
     exit(1);
 }
-/*
-static void test_fail(char *file, int line, char *call, int retval){
-    printf("%s\tFAILED\nLine # %d\n", file, line);
-    if ( retval == PAPI_ESYS ) {
-        char buf[128];
-        memset( buf, '\0', sizeof(buf) );
-        sprintf(buf, "System error in %s:", call );
-        perror(buf);
-    }
-    else if ( retval > 0 ) {
-        printf("Error calculating: %s\n", call );
-    }
-    else {
-        printf("Error in %s: %s\n", call, PAPI_strerror(retval) );
-    }
-    printf("\n");
-    exit(1);
-}
-*/
 
 //мб вообще не надо использовать массов С
 void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::vector<float>>& B, int counter_num)
 {
-	
 	std::size_t block_size;
 	std::vector<std::vector<float>> C(size, std::vector<float>(size, 0));//
-	if (counter_num >= 0 and counter_num < 4) {
-		int first_group_of_events[4] {PAPI_TOT_CYC, PAPI_L1_TCM, PAPI_L2_TCM, PAPI_TLB_TL};
-		long long values[1];
-		int Event[1] = first_group_of_events[counter_num];
-		
+	if (counter_num == 0 or counter_num == 1) {
+		int first_group_of_events[4] {PAPI_TOT_CYC, PAPI_TLB_TL, PAPI_L1_TCM, PAPI_L2_TCM};
+		long long values[NUM_EVENTS];
+		int Events[NUM_EVENTS];
+		std::string first_event, second_event;
+		if (counter_num == 0) {
+			first_event = "CPU cycles";
+			Events[0] = first_group_of_events[0];
+			second_event = "Total translation lookaside buffer misses";
+			Events[1] = first_group_of_events[1];
+		} else {
+			first_event = "Level 1 cache misses";
+			Events[0] = first_group_of_events[2];
+			second_event = "Level 2 cache misses";
+			Events[1] = first_group_of_events[3];
+		}
+		std::cout << first_event << "\t" << second_event << std::endl;;
 		//32*32 ijk
 		block_size = 32;
-		PAPI_start_counters(Event, 1);
+		PAPI_start_counters(Events, NUM_EVENTS);
 		for (std::size_t i {0}; i < size; i+= block_size) {
 			for (std::size_t j {0}; j < size; j += block_size) {
 				for (std::size_t k {0}; k < size; k += block_size) {
@@ -57,12 +50,12 @@ void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::
 				}
 			}
 		}
-		PAPI_stop_counters(values, 1);
-		std::cout << "ijk 32*32 - " << values[0] << std::endl;
+		PAPI_stop_counters(values, NUM_EVENTS);
+		std::cout << values[0] << "\t" << values[1] << "\t" << " - ijk 32*32" << std::endl;
 		
 		//32*32 ikj
 		block_size = 32;
-		PAPI_start_counters(Event, 1);
+		PAPI_start_counters(Events, NUM_EVENTS);
 		for (std::size_t i {0}; i < size; i+= block_size) {
 			for (std::size_t k {0}; k < size; k += block_size) {
 				for (std::size_t j {0}; j < size; j += block_size) {
@@ -76,18 +69,18 @@ void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::
 				}
 			}
 		}
-		PAPI_stop_counters(values, 1);
-		std::cout << "ikj 32*32 - " << values[0] << std::endl;
+		PAPI_stop_counters(values, NUM_EVENTS);
+		std::cout << values[0] << "\t" << values[1] << "\t" << " - ikj 32*32" << std::endl;
 		
 		//b*b ikj
 		//3 * b * b = 256 * 1024 / 16 (L2)  ~73.9
 		//3 * b * b = 32 * 1024 / 16 (L1) ~26.1
- 		if (counter_num == 1) {
+ 		if (counter_num == 1) {//L1 cache misses
  			block_size = 24;
- 		} else {
+ 		} else {/////////////////////////////////////////////////////////////////////////разделить в два разных L1 i L2
  			block_size = 72;
  		}
-		PAPI_start_counters(Event, 1);
+		PAPI_start_counters(Events, NUM_EVENTS);
 		for (std::size_t i {0}; i < size; i+= block_size) {
 			for (std::size_t k {0}; k < size; k += block_size) {
 				for (std::size_t j {0}; j < size; j += block_size) {
@@ -101,10 +94,10 @@ void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::
 				}
 			}
 		}
-		PAPI_stop_counters(values, 1);
-		std::cout << "ikj b*b - " << values[0] << std::endl;
+		PAPI_stop_counters(values, NUM_EVENTS);
+		std::cout << values[0] << "\t" << values[1] << "\t" << " - ikj " << block_size << "*" << block_size << std::endl;
 	} else {
-
+		std::cout << "Proc time" << "\t" << "MFLOP" << std::endl;
 		float real_time, proc_time, mflops;
 		long long flpins;
 		//32*32 ijk
@@ -124,11 +117,7 @@ void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::
 			}
 		}
 		PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
-		if (counter_num == 4) {
-			std::cout << "Proc time - " << proc_time << std::endl;
-		} else {
-			std::cout << "MFLOP - " << mflops << std::endl;
-		}
+		std::cout << proc_time << "\t" << mflops << "\t" << " - ijk 32*32" << std::endl;
 		PAPI_shutdown();
 
 		//32*32 ikj
@@ -148,20 +137,13 @@ void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::
 			}
 		}
 		PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
-		if (counter_num == 4) {
-			std::cout << "Proc time - " << proc_time << std::endl;
-		} else {
-			std::cout << "MFLOP - " << mflops << std::endl;
-		}
+		std::cout << proc_time << "\t" << mflops << "\t" << " - ikj 32*32" << std::endl;
+		PAPI_shutdown();
 
 		//b*b ikj
 		//3 * b * b = 256 * 1024 / 16 (L2)  ~73.9
 		//3 * b * b = 32 * 1024 / 16 (L1) ~26.1
- 		if (counter_num == 1) {
- 			block_size = 24;
- 		} else {
- 			block_size = 72;
- 		}
+ 		block_size = 72;
 		PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
 		for (std::size_t i {0}; i < size; i+= block_size) {
 			for (std::size_t k {0}; k < size; k += block_size) {
@@ -177,11 +159,8 @@ void mul(std::size_t size, std::vector<std::vector<float>>& A, std::vector<std::
 			}
 		}
 		PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
-		if (counter_num == 4) {
-			std::cout << "Proc time - " << proc_time << std::endl;
-		} else {
-			std::cout << "MFLOP - " << mflops << std::endl;
-		}
+		std::cout << proc_time << "\t" << mflops << " - ijk " << block_size << "*" << block_size << std::endl;
+
 	}
 }
 /*
@@ -216,13 +195,10 @@ int main(int argc, char* argv[])
 			B[i][j] = tmp;
 		}
 	}
-	std::cout << "Choose counter number:" << std::endl
-				<< "0 - CPU cycles" << std::endl
-				<< "1 - Level 1 cache misses" << std::endl
-				<< "2 - Level 2 cache misses" << std::endl
-				<< "3 - Total translation lookaside buffer misses" << std::endl
-				<< "4 - Proc ime" << std::endl
-				<< "5 - MFLOP" << std::endl;
+	std::cout << "Choose counter pair number:" << std::endl
+				<< "0 - CPU cycles / Total translation lookaside buffer misses" << std::endl
+				<< "1 - Level 1 cache misses / Level 2 cache misses" << std::endl
+				<< "2 - Proc time / MFLOP" << std::endl;
 	int num;
 	std::cin >> num;
 	mul(size, A, B, num);
--- Task2/tmp.cpp
+++ Task2/tmp.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
-#include <papi.h>
+#include "papi.h"
 #include <vector>
 
 int main() {
 	float real_time, proc_time, mflops;
 	long long flpins;
-	int Events[2] = {PAPI_TOT_CYC, PAPI_FP_OPS};
-	long long values[2];
-	
-	std::size_t size {600};
-	std::vector<std::vector<float>> A(size, std::vector<float>(size, 0)),
-									B(size, std::vector<float>(size, 0)),
+	std::size_t size {1000};
+	std::vector<std::vector<float>> A(size, std::vector<float>(size, 1)),
+									B(size, std::vector<float>(size, 2)),
 									C(size, std::vector<float>(size, 0));
 	//PAPI_start_counters(Events, 2);
-	PAPI_flops( &real_time, &proc_time, &flpins, &mflops);
-	for (std::size_t m {0}; m < size; ++m) {
-		for (std::size_t n {0}; n < size; ++n) {
-			for (std::size_t l {0}; l < size; ++l) {
-				C[m][n] += A[m][l] * B[l][n];
+	std::size_t block_size = 70;
+	PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
+	for (std::size_t i {0}; i < size; i+= block_size) {
+		for (std::size_t j {0}; j < size; j += block_size) {
+			for (std::size_t k {0}; k < size; k += block_size) {
+				for (std::size_t m {i}; m < i + block_size and m < size; ++m) {
+					for (std::size_t n {j}; n < j + block_size and n < size; ++n) {
+						for (std::size_t l {k}; l < k + block_size and l < size; ++l) {
+							C[m][n] += A[m][l] * B[l][n];
+						}
+					}
+				}
 			}
 		}
 	}
-	//PAPI_stop_counters(values, 2);
-	PAPI_flops( &real_time, &proc_time, &flpins, &mflops);
+	PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
 	std::cout << real_time << std::endl << proc_time << std::endl << flpins << std::endl << mflops << std::endl;
 	PAPI_shutdown();
-	PAPI_flops( &real_time, &proc_time, &flpins, &mflops);
-	for (std::size_t m {0}; m < size; ++m) {
-		for (std::size_t n {0}; n < size; ++n) {
-			for (std::size_t l {0}; l < size; ++l) {
-				C[m][n] += A[m][l] * B[l][n];
-			}
-		}
-	}
-	//PAPI_stop_counters(values, 2);
-	PAPI_flops( &real_time, &proc_time, &flpins, &mflops);
-	std::cout << real_time << std::endl << proc_time << std::endl << flpins << std::endl << mflops << std::endl;
-	//std::cout << "tot_cyc - " << values[0] << std::endl
-	//			<< "fp_ops - " << values[1] << std::endl;
-
 	return 0;
 }
